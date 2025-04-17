8f15b5069fac3ef43114d4446ae0d15f2a6fddae
hajasko <hajasko87@gmail.com>
2025-04-17 03:32:24 +0200

    műveleti függvények beépítése az eseménykezelőkbe

--- calculator.js
+++ calculator.js
@@ -6,36 +6,11 @@ function displayResult(operationSymbol, operationFunction) {
     document.querySelector(".js-container").innerText = `${ firstNumber } ${operationSymbol} ${ secondNumber } = ${result}`;
 }
 
-function displaySum() {
-    let sum = (a, b) => a + b;
-    displayResult('+', sum)
-  }
-  
-  function displayDiff() {
-    let diff = (a, b) => a - b; 
-    displayResult('-', diff);
-  }
-
-  function displayProd() {
-   let prod = (a, b) => a * b;
-   displayResult('*', prod);
-  }
-
-  function displayDiv() {
-    let div = (a, b) => a / b;
-    displayResult('/', div );
-  }
-
-  function displayMod() {
-    let mod = (a, b) => a % b;
-    displayResult('%', mod);
-  }
-
-  document.querySelector(".js-plus").addEventListener("click", displaySum);
-  document.querySelector(".js-minus").addEventListener("click", displayDiff);
-  document.querySelector(".js-times").addEventListener("click", displayProd);
-  document.querySelector(".js-division").addEventListener("click", displayDiv);
-  document.querySelector(".js-modulus").addEventListener("click", displayMod);
+  document.querySelector(".js-plus").addEventListener("click", () => displayResult('+', (a, b) => a + b));
+  document.querySelector(".js-minus").addEventListener("click", () => displayResult('-', (a, b) => a - b));
+  document.querySelector(".js-times").addEventListener("click", () => displayResult('*', (a, b) => a * b));
+  document.querySelector(".js-division").addEventListener("click", () => displayResult('/', (a, b) => a / b ));
+  document.querySelector(".js-modulus").addEventListener("click", () => displayResult('%', (a, b) => a % b));
   
   function formSubmitted(event) {
     event.preventDefault();
